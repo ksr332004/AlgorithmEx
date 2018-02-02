ff0cd908c715639178a68566c0c5a075058a97de
seran <ksr332004@gmail.com>
2018-02-02 11:34:19 +0900

    [sw expert academy] 1949. [모의 SW 역량테스트] 등산로 조성 풀이 시작

---
+++ src/algorithm/samsung/search/Problem1949.java
@@ -0,0 +1,48 @@
+package algorithm.samsung.search;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * @author Seran
+ * 1949. [모의 SW 역량테스트] 등산로 조성
+ */
+public class Problem1949 {
+
+	public static void main(String[] args) throws Exception {
+		String myFile = Problem1949.class.getResource("").getPath();
+		File file = new File(myFile + "../input/", "input.txt");
+		Scanner sc = new Scanner(file);
+		
+		int T = sc.nextInt();
+		for(int test_case=0; test_case<T; T++) {
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			
+			int[][] M = new int[N][N];
+			int max = 0;
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					M[i][j] = sc.nextInt();
+					if(max < M[i][j]) {
+						max = M[i][j];
+					}
+				}
+			}
+			
+			int Answer = 0;
+			int[][] L = new int[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					if(max == L[i][j]) {
+						
+					}
+				}
+			}
+			
+			System.out.println("#" + (test_case+1) + " ");
+		}
+
+	}
+
+}
