03f19c7cdbe8dc9ddbd698b1f084f9e15178a9f3
seran <ksr332004@gmail.com>
2018-02-20 10:59:06 +0900

    [sw expert academy] 2817. 부분 수열의 합 풀이 완료

--- src/algorithm/samsung/search/Problem2817.java
+++ src/algorithm/samsung/search/Problem2817.java
@@ -11,6 +11,25 @@ import java.util.Scanner;
  * 재귀함수 이용
  */
 public class Problem2817 {
+    static int answer;
+
+    static void recursive(int[] A,int idx, int K, int sum) {
+        // 수열 A를 더한 sum 값과 수열의 합 결과 K값이 같으면 경우의 수를 +1
+        if(sum == K) {
+            answer++;
+            return;
+        }
+
+        // 수열 A를 Index 0번부터 수차적으로 더해가면서
+        // 더한 sum 값이 수열의 합 결과 K값 보다 작거나 같으면 recursive() 함수를 호출
+        for(int i=idx; i<A.length; i++) {
+            if(sum + A[i] > K) {
+                continue;
+            } else {
+                recursive(A, i+1, K, sum+A[i]);
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception{
         String myFile = Problem2817.class.getResource("").getPath();
@@ -19,7 +38,7 @@ public class Problem2817 {
 
         int T = sc.nextInt();
         for(int test_case=1; test_case<=T; test_case++) {
-            int answer = 0;
+            answer = 0;
 
             // 수열의 크기 N, 수의 합 결과 K
             int N = sc.nextInt();
@@ -30,6 +49,7 @@ public class Problem2817 {
                 A[i] = sc.nextInt();
             }
 
+            recursive(A, 0, K, 0);
 
             System.out.println("#" + test_case + " " + answer);
         }
