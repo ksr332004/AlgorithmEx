437af013b8d7ded54775500eb575fb74580eaefa
Seran <ksr332004@gmail.com>
2018-04-10 01:07:39 +0900

    [sw expert academy] 1953. [모의 SW 역량테스트] 탈주범 검거 풀이 시작

---
+++ src/algorithm/samsung/graph/bfs/Problem1953.java
@@ -0,0 +1,22 @@
+package algorithm.samsung.graph.bfs;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * @author Seran
+ * 1953. [모의 SW 역량테스트] 탈주범 검거
+ */
+
+public class Problem1953 {
+
+	public static void main(String[] args) throws Exception {
+		String myFile = Problem1953.class.getResource("").getPath();
+		File file = new File(myFile + "../input", "input.txt");
+		
+		Scanner sc = new Scanner(file);
+		
+		
+	}
+
+}
